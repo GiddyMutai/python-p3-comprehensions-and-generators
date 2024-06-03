34cdc8dd1f2a6ee36edd717c0108aea0cba4eae9
Gideon Mutai <giddymutai13@gmail.com>
2024-06-03 23:35:39 +0300

    FInished task

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if len(num_list) == 0:
+        return []
+    else: 
+        return [num for num in num_list if num%2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return []
+    else:
+        return [string + '!' for string in sentence_list]
